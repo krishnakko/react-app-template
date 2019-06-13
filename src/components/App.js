dd27d9e544250db6b3ab2df4b1697035805c4ee0
Venkata Krishna Reddy Peram <krishna971.in@gmail.com>
2019-06-13 17:48:40 +0530

    This is a basic react app template which helps to create any react app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+//Creating a react component
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App; // allows the other files to import this Component
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';  //Importing Reactjs
+import ReactDOM from 'react-dom';  //helps for insertion with the core directory
+import App from './components/App';
+import './index.css';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
